9858229ab8d6c7a430c6e6dc0df2204e26ccc286
Josiah Roberts <robertjo@asynchrony.com>
2017-10-24 09:23:02 -0500

    added only support - JR WL

--- lib/ToqAva.ts
+++ lib/ToqAva.ts
@@ -2,14 +2,20 @@ import { Fixture } from './Fixture'
 import test from 'ava'
 declare var v8debug: any;
 
-export function avaFixture<TClass>(constructor: new(...dependencies: Array<any>) => TClass, ...depedencyTypes: Array<new () => any>) {
-    return function(target: any) {
-        let type = target as new(constructor: new(...dependencies: Array<any>) => TClass, ...dependencyTypes: Array<any>) => Fixture<TClass>;
-
+export function avaFixture<TClass>(constructor: new (...dependencies: Array<any>) => TClass, ...depedencyTypes: Array<new () => any>) {
+    return function (target: any) {
+        let type = target as new (constructor: new (...dependencies: Array<any>) => TClass, ...dependencyTypes: Array<any>) => Fixture<TClass>;
         let register = type.prototype as TestRegister<TClass>;
-        for(var registration of register.testRegistrations) {
+        for (var registration of register.testRegistrations) {
+            let isOnly = (register.onlyTests != undefined && register.onlyTests.indexOf(registration.methodName) != -1);
             let fixture = new type(constructor, ...depedencyTypes) as any;
-            test((type as any).name + ": " + registration.testName, fixture[registration.methodName].bind(fixture));
+            let testName = (type as any).name + ": " + registration.testName;
+
+            if (isOnly) {
+                test.only(testName, fixture[registration.methodName].bind(fixture));
+            } else {
+                test(testName, fixture[registration.methodName].bind(fixture));
+            }
         }
     }
 }
@@ -21,6 +27,12 @@ export function avaTest<TClass, T extends Fixture<TClass>>(target: T, propertyKe
     }
 
     register.testRegistrations.push(new TestRegistration(propertyKey));
+}
+
+export function only<TClass, T extends Fixture<TClass>>(target: T, propertyKey: string, descriptor: any) {
+    let register = target as any as TestRegister<TClass>
+    register.onlyTests = register.onlyTests || [];
+    register.onlyTests.push(propertyKey);
 }
 
 class TestRegistration {
@@ -35,4 +47,6 @@ class TestRegistration {
 
 interface TestRegister<TClass> extends Fixture<TClass> {
     testRegistrations: Array<TestRegistration>;
+    isOnly: boolean;
+    onlyTests: Array<string>;
 }
